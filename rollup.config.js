20b1483869dc4652b6fcfb75fe662bbda62292c8
Jonathan R <veto1024@gmail.com>
2023-02-11 11:10:55 -0500

    Added MyFormikStuff

---
+++ rollup.config.js
@@ -0,0 +1,44 @@
+import resolve from '@rollup/plugin-node-resolve';
+import typescript from 'rollup-plugin-typescript2';
+import terser from '@rollup/plugin-terser';
+import replace from '@rollup/plugin-replace';
+import pkg from './package.json';
+
+export default {
+	input: 'src/index.tsx',
+	output: [{
+		format: 'esm',
+		file: pkg.module,
+		sourcemap: false,
+	}, {
+		format: 'cjs',
+		file: pkg.main,
+		sourcemap: false,
+		esModule: false,
+	}, {
+		name: pkg['umd:name'] || pkg.name,
+		format: 'umd',
+		file: pkg.unpkg,
+		sourcemap: false,
+		esModule: false,
+		plugins: [
+			terser()
+		]
+	}],
+	external: [
+		...require('module').builtinModules,
+		'react/jsx-runtime',
+		...Object.keys(pkg.dependencies || {}),
+		...Object.keys(pkg.peerDependencies || {}),
+	],
+	plugins: [
+		resolve({modulesOnly: true}),
+		replace(),
+		terser(),
+		typescript({
+			useTsconfigDeclarationDir: true,
+			exclude: 'node_modules'
+		})
+	],
+	exclude:'node_modules'
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,6 @@
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {BrowserRouter} from "react-router-dom";
-import {HelmetProvider} from "react-helmet-async";
-import {CookiesProvider} from "react-cookie";
-import { createRoot } from 'react-dom/client';
 
 const container = document.getElementById('root');
 const root = ReactDOM.createRoot(container as HTMLElement);
